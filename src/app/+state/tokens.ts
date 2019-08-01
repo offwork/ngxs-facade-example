a32da756e11d682d0aa802e251297b6f70b6ad5c
offwork <chomunist@gmail.com>
2019-08-01 22:42:06 +0300

    Auto-generated commit

--- src/app/+state/tokens.ts
+++
@@ -1,3 +0,0 @@
-import { InjectionToken } from '@angular/core';
-
-export const BASE_URL: InjectionToken<string> = new InjectionToken('BaseUrl');
--- src/app/shared/shared.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { DesingModule } from './design.module';
-
-@NgModule({
-  imports: [ DesingModule ],
-  exports: [DesingModule],
-})
-export class SahredModule {}
